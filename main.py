e1e72b90c9cce19bf205097a7ec61b9f6a246c13
Kwischan <82772369+Kwischan@users.noreply.github.com>
2026-01-31 15:39:49 +0000

    tkinter test

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Capybara Test 🦫")
+root.geometry("300x200")
+
+label = tk.Label(root, text="Capybara is chilling 😌")
+label.pack(pady=40)
+
+root.mainloop()
